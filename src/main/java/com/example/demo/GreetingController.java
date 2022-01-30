f6624321f39b631ae416ec787f4001bea3134fde
1cCoder <chiraj.atwork@gmail.com>
2022-01-30 22:51:30 +0530

    Sudoku Solver

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -10,12 +10,15 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		
 		model.addAttribute("name", name);
+		model.addAttribute("sudoku", new SudokuSolver().sudoku());
 		return "greeting";
 	}
 	
-//	@GetMapping("/")
-//	public String index() {
+//	@GetMapping("/sudoku")
+//	public String index(Model model) {
+//		model.addAttribute("name", name);
 //		return "index";
 //	}
 
---
+++ src/main/java/com/example/demo/SudokuBoard.java
@@ -0,0 +1,86 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+public class SudokuBoard {
+
+
+	private int[][] board;
+	private String resultMessage;
+
+
+	/**
+	 * 
+	 */
+	public SudokuBoard() {
+		super();
+	}
+
+	/**
+	 * @param board
+	 */
+	public SudokuBoard(int[][] board) {
+		super();
+		this.board = board;
+	}
+
+	/**
+	 * @return the board
+	 */
+	public int getValue(int row, int col) {
+		return board[row][col];
+	}
+
+	/**
+	 * @return the board
+	 */
+	public int[][] getArray() {
+		return board;
+	}
+
+	/**
+	 * @param board the board to set
+	 */
+	public void setValue(int[][] board) {
+		this.board = board;
+	}
+
+	/**
+	 * @param board the board to set
+	 */
+	public void setValue(int row, int col, int number) {
+		this.board[row][col] = number;
+	}
+
+	/**
+	 * @return the resultMessage
+	 */
+	public String getResultMessage() {
+		return resultMessage;
+	}
+
+	/**
+	 * @param resultMessage the resultMessage to set
+	 */
+	public void setResultMessage(String resultMessage) {
+		this.resultMessage = resultMessage;
+	}
+
+	@Override
+	public String toString() {
+		return "SudokuBoard [board=" + boardString(board) + ", resultMessage=" + resultMessage + "]";
+	}
+
+	private String boardString(int[][] board2) {
+		// TODO Auto-generated method stub
+		StringBuilder stb = new StringBuilder();
+		for(int[] arr : board2) {
+			for(int val : arr) {
+				stb.append(val + " ");
+			}
+			stb.append(",");
+		}
+		return stb.toString();
+	}
+
+}
