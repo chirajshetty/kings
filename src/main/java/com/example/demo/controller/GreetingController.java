b5e31f7f73df3caf177531faf923648389f1dc4f
1cCoder <chiraj.atwork@gmail.com>
2022-02-13 22:17:02 +0530

    prop file

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -17,9 +17,9 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
 			Model model) {
-		model.addAttribute(Messages.getString("GreetingController.0"), name); //$NON-NLS-1$
-		model.addAttribute(Messages.getString("GreetingController.1"), sudokuService.solveSudoku()); //$NON-NLS-1$
-		return Messages.getString("GreetingController.2"); //$NON-NLS-1$
+		model.addAttribute("name", name);
+		model.addAttribute("sudoku", sudokuService.solveSudoku());
+		return "greeting";
 	}
 
 }
